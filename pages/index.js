6ef25c97a540cd66d38e93842906ee274e6047de
luthfib <luthfi.bustillos@joinaltitude.com>
2025-03-29 15:10:12 +0700

    update to have favicon

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,25 @@ class Index extends Component {
     return (
       <div>
         <Head>
+          <link rel="icon" href="/static/images/favicon.ico" />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link
+            rel="icon"
+            type="image/svg+xml"
+            href="/static/images/favicon.svg"
+          />
+          <link rel="shortcut icon" href="/static/images/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/images/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/static/images/site.webmanifest" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
           <meta
