0d26d621ff6ed1c4c6167d64ed8786d7d78733f6
luthfib <luthfibustillos@gmail.com>
2026-03-01 14:08:42 +0700

    more fixes

--- src/lib/components/logo.svelte
+++ src/lib/components/logo.svelte
@@ -5,7 +5,6 @@
 <enhanced:img
   alt="Brand"
   {src}
-  {height}
   class="object-contain {compact ? 'pr-3 w-auto' : 'pr-6 w-[150px] md:pl-12'}"
   style="height: {height}; max-height: {compact ? '80px' : '175px'};"
 />
