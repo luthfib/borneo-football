db373ff99034b1e675ef751ca892bc57b88937a9
luthfib <luthfibustillos@gmail.com>
2026-03-01 14:28:05 +0700

    slight change to force push

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,9 +1,9 @@
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
-	const response = await resolve(event);
+  const response = await resolve(event);
 
-	// Ensure search engines can index the production site
-	response.headers.set('X-Robots-Tag', 'index, follow');
+  // Ensure search engines can index the production site
+  response.headers.set("X-Robots-Tag", "index, follow");
 
-	return response;
+  return response;
 }
